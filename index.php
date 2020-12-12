d02cb0fdb459b8cdaa8dbc8bceb6036b5f5c77f8
Евгений <75871425+galant1s@users.noreply.github.com>
2020-12-12 20:06:51 +0500

    first upload
    1 step to create good project

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/jQuery.Brazzers-Carousel.min.css">
+  <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+  <title>Убийца avito</title>
+</head>
+<body>
+
+  <header>
+    <div class="head">
+      <img src="img/logo.svg" alt="logo" style="float:left">
+      <div class="category">
+        <ul class="hr">
+          <li>Недвижимость</li>
+          <li>Авто</li>
+          <li>Бытовая техника</li>
+          <li>Ещё</li>
+        </ul>
+      </div>
+    </div>
+  </header>
+
+  <section class="gallery-preview">
+    <div class="gallery">
+      <div class="container">
+
+            <div class="item">
+              <img src="http://3.bp.blogspot.com/-Ah66MLalGlU/T8uvFqruVcI/AAAAAAAAAXE/LLnaIGsWn2U/s320/wallpaper-desktop-widescreen-3.jpg" alt="Alt">
+              <img src="http://3.bp.blogspot.com/-ZkYmM4G7u-k/UXKMPkHa1rI/AAAAAAAAEYI/B_O0hue3T0w/s320/welcome+wallpaper.jpg" alt="Alt">
+              <img src="http://2.bp.blogspot.com/_v5h2qxDB1io/TPuFscjEIUI/AAAAAAAAAYY/gXNXE3MRblU/s320/Christmas+HD+Wallpapers+1920x1200+%252879%2529.jpg" alt="Alt">
+              <img src="http://4.bp.blogspot.com/-E4kDFZoBkO8/UB1a0OOxZXI/AAAAAAAAADw/6dtng9D-uFE/s320/laptop-wallpapers-widescreen-2.jpg" alt="Alt">
+            </div>
+
+      </div>
+
+      <div class="container">
+          
+            <div class="item">
+              <img src="http://3.bp.blogspot.com/-Ah66MLalGlU/T8uvFqruVcI/AAAAAAAAAXE/LLnaIGsWn2U/s320/wallpaper-desktop-widescreen-3.jpg" alt="Alt">
+              <img src="http://3.bp.blogspot.com/-ZkYmM4G7u-k/UXKMPkHa1rI/AAAAAAAAEYI/B_O0hue3T0w/s320/welcome+wallpaper.jpg" alt="Alt">
+              <img src="http://2.bp.blogspot.com/_v5h2qxDB1io/TPuFscjEIUI/AAAAAAAAAYY/gXNXE3MRblU/s320/Christmas+HD+Wallpapers+1920x1200+%252879%2529.jpg" alt="Alt">
+              <img src="http://4.bp.blogspot.com/-E4kDFZoBkO8/UB1a0OOxZXI/AAAAAAAAADw/6dtng9D-uFE/s320/laptop-wallpapers-widescreen-2.jpg" alt="Alt">
+            </div>
+        
+      </div>
+
+      <div class="container">
+      
+            <div class="item">
+              <img src="http://3.bp.blogspot.com/-Ah66MLalGlU/T8uvFqruVcI/AAAAAAAAAXE/LLnaIGsWn2U/s320/wallpaper-desktop-widescreen-3.jpg" alt="Alt">
+              <img src="http://3.bp.blogspot.com/-ZkYmM4G7u-k/UXKMPkHa1rI/AAAAAAAAEYI/B_O0hue3T0w/s320/welcome+wallpaper.jpg" alt="Alt">
+              <img src="http://2.bp.blogspot.com/_v5h2qxDB1io/TPuFscjEIUI/AAAAAAAAAYY/gXNXE3MRblU/s320/Christmas+HD+Wallpapers+1920x1200+%252879%2529.jpg" alt="Alt">
+              <img src="http://4.bp.blogspot.com/-E4kDFZoBkO8/UB1a0OOxZXI/AAAAAAAAADw/6dtng9D-uFE/s320/laptop-wallpapers-widescreen-2.jpg" alt="Alt">
+            </div>
+      
+      </div>
+
+
+      <script src="js/jQuery.Brazzers-Carousel.min.js"></script>
+
+      <script>
+      $(document).ready(function() {
+        $(".item").brazzersCarousel();
+      });
+      </script>
+    </section>
+
+
+    <footer>
+      <div class="contact">
+        <span>Проект Avito</span>
+      </div>
+    </footer>
+  </body>
+  </html>
